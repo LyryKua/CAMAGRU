7ad96cfe01db20e653eed5b67d2babad935ffa84
Kyrylo Hrecheniuk <khrechen@e1r2p8.unit.ua>
2017-08-20 14:11:12 +0300

    test

---
+++ Core/Router.class.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Class Router
+ */
+
+namespace Core;
+
+class Router
+{
+    /**
+     * Associative array of routes (the routing table)
+     * 'route' => ['controller' => 'action']
+     *
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Parameters from the matched route
+     *
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route The route URL
+     * @param array $params Parameters (controller, action)
+     * @return void
+     */
+    public function add($route, $params = [])
+    {
+        echo "0) " . $route . "<br>";
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+        echo "1) " . $route . "<br>";
+
+        // Convert variables e.g. {controller}
+		$route = preg_replace('/\{([\da-z-]+)\}/', '(?P<\1>[\da-z-]+)', $route);
+        echo "2) " . $route . "<br>";
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+        echo "3) " . $route . "<br>";
+
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Match the routing to the routes int the routing table,
+     *
+     * @param string $url The URL
+     * @return bool
+     */
+    public function match($url)
+    {
+        /*
+//        foreach ($this->routes as $route => $value) {
+//            if ($url == $route) {
+//                $this->params = $value;
+//                return true;
+//            }
+//        }
+//        return false;
+
+//        $reg_exp = '/^(?P<controller>[\da-z-]+)\/(?P<action>[\da-z-]+)$/';
+//        if(preg_match($reg_exp, $url, $matches)) {
+//            $params = [];
+//            foreach ($matches as $key => $match) {
+//                if (is_string($key)) {
+//                    $params[$key] = $match;
+//                }
+//            }
+//            $this->params = $params;
+//            return true;
+//        }
+//        return false;
+        */
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns an array params
+     *
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+}
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Front controller
+ */
+
+require 'Core/Router.class.php';
+
+$router = new Core\Router();
+$url = $_SERVER['QUERY_STRING'];
+
+// Adding routes to the routing table
+//$router->add('', ['controller' => 'Home', 'action' => 'index']);
+//$router->add('sign-up', ['controller' => 'AuthorizationModel', 'action' => 'signUp']);
+//$router->add('log-in', ['controller' => 'AuthorizationModel', 'action' => 'logIn']);
+//$router->add('log-out', ['controller' => 'AuthorizationModel', 'action' => 'logOut']);
+//$router->add('user', ['controller' => 'User', 'action' => 'index']);
+//$router->add('user/add', ['controller' => 'User', 'action' => 'add']);
+//$router->add('user/del', ['controller' => 'User', 'action' => 'del']);
+//$router->add('user/like', ['controller' => 'User', 'action' => 'like']);
+$router->add('{controller}/{action}');
+
+if ($router->match($url)) {
+    echo "<pre>";
+    print_r($router->getParams());
+    echo "</pre>";
+} else {
+    echo "<h1>404</h1>";
+}
