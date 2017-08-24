9684116380de8190efcd9acbcf0de241f354fbe1
Kyrylo Hrecheniuk <khrechen@e3r6p3.unit.ua>
2017-08-24 20:45:08 +0300

    Independents day

--- App/Views/index.php
+++ App/Views/index.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>camagru</title>
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
 </head>
 <body>
 <header>
-    <div><img src="/App/Views/icons/add_new_photo.png"></div>
+    <div class="first">
+        <img src="icons/add_new.svg">
+    </div>
+    <div class="second">camagru</div>
+    <div class="third"><img src="icons/user.svg"></div>
 </header>
-<div>photo</div>
-<div>photo</div>
-<div>photo</div>
-<div>photo</div>
-<div>photo</div>
-<div><img src=""><?php echo __DIR__; ?></div>
 </body>
 </html>
