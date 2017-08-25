0cb25f8ef35f502980bfcb2d3bfc1034cb257902
Kyrylo Hrecheniuk <khrechen@e3r6p3.unit.ua>
2017-08-25 21:30:30 +0300

    xyz

--- App/Views/blocks/footer.php
+++ App/Views/blocks/footer.php
@@ -1,3 +1,13 @@
 <footer>
-footer
+	<div class="left">
+		<p>Copyright © 2017 khrechen<br></p>
+	</div>
+	<div class="right">
+		<ul>
+            <li><a href="https://github.com/LyryKua" target="_blank"><img src="icons/sociable/github.svg" alt="GitHub"></a></li>
+            <li><a href="https://www.linkedin.com/in/kyrylo-hrecheniuk-30ba29109/" target="_blank"><img src="icons/sociable/linkedin.svg" alt="LinkedIn"></a></li>
+            <li><a href="https://www.facebook.com/kyrylo.hrecheniuk target="_blank""><img src="icons/sociable/fb.svg" alt="Facebook"></a></li>
+            <li><a href="https://twitter.com/LyryKua" target="_blank"><img src="icons/sociable/twitter.svg" alt="Twitter"></a</li>
+		</ul>
+	</div>
 </footer>
--- App/Views/index.php
+++ App/Views/index.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../../public/styles/blocks/header.css">
-    <link rel="stylesheet" type="text/css" href="../../public/styles/html.css">
+    <link rel="stylesheet" type="text/css" href="/styles/blocks/header.css">
+    <link rel="stylesheet" type="text/css" href="/styles/blocks/footer.css">
+    <link rel="stylesheet" type="text/css" href="/styles/html.css">
     <title>camagru</title>
 </head>
 <body>
     <?php require_once('blocks/header.php'); ?>
-    <p> CONTENT </p>
+	<?php require_once('blocks/content.php'); ?>
     <?php require_once('blocks/footer.php'); ?>
 </body>
 </html>
