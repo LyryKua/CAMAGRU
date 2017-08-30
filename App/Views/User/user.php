13652ec4b93b16a51bf9fd98a05bbbd22c78884b
Kyrylo Hrecheniuk <khrechen@e3r6p3.unit.ua>
2017-08-30 18:47:43 +0300

    jj

--- App/Views/User/user.php
+++ App/Views/User/user.php
@@ -9,7 +9,8 @@
 <?php require(dirname(__FILE__) . '/../blocks/header.php'); ?>
 <?php
 echo "<p>Hello, " . $users[0]['name'] . " " . $users[0]['surname'] . "<br>";
-echo "I know your email: " . $users[0]['email'] . "</p>"
+echo "I know your email: " . $users[0]['email'] . "</p>";
+echo "<pre>" . var_dump($users[0]['registration_date']) . "</pre>";
 ?>
 <?php require(dirname(__FILE__) . '/../blocks/footer.php'); ?>
 </body>
