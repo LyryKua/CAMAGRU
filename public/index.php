9c9f095e7fdb232f2be51830a4fbb9829a54ae4b
Kyrylo Hrecheniuk <khrechen@e3r6p3.unit.ua>
2017-09-06 16:07:12 +0300

    public

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Front controller
+ */
+
+
+session_start();
+
+/*
+ * Autoloader
+ */
+spl_autoload_register(function ($class) {
+	$root = dirname(__DIR__); // get the parent directory
+	$file = $root . '/' . str_replace('\\', '/', $class) . '.class.php';
+	if (is_readable($file)) {
+		require $root . '/' . str_replace('\\', '/', $class) . '.class.php';
+	}
+});
+
+$router = new Core\Router();
+$url = trim($_SERVER['QUERY_STRING'], '/');
+
+// Adding routes to the routing table
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('sign-up', ['controller' => 'Authorization', 'action' => 'signUp']);
+$router->add('log-in', ['controller' => 'Authorization', 'action' => 'logIn']);
+$router->add('log-out', ['controller' => 'Authorization', 'action' => 'logOut']);
+$router->add('user', ['controller' => 'User', 'action' => 'index']);
+$router->add('reset-password', ['controller' => 'Authorization', 'action' => 'resetPassword']);
+$router->add('{controller}/{action}');
+//$router->add('{controller}/{id}/{action}');
+
+$router->dispatch($url);
