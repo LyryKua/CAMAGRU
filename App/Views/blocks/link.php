8c3b22acfd1e4db9e925cba584be24eaeeaff890
Kyrylo Hrecheniuk <khrechen@e3r6p3.unit.ua>
2017-09-06 15:56:03 +0300

    Notrmalize comments and cod

--- App/Views/blocks/link.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/styles/blocks/header.css">
-<link rel="stylesheet" type="text/css" href="/styles/blocks/footer.css">
-<link rel="stylesheet" type="text/css" href="/styles/html.css">
--- Core/Model.class.php
+++ Core/Model.class.php
@@ -18,6 +18,13 @@ use App\Configuration;
  */
 abstract class Model
 {
+	/**
+	 * getDB()
+	 *
+	 * Get the PDO database connection.
+	 *
+	 * @return null|PDO
+	 */
 	protected static function getDB()
 	{
 		static $db = null;
@@ -32,8 +39,77 @@ abstract class Model
 		return $db;
 	}
 
+	/**
+	 * getUserById($user_id)
+	 *
+	 * This function get an user by `user_id` in DB's table.
+	 *
+	 * The return value of this function is a row as an array indexed by column name.
+	 * Function returns false, if the user was not found
+	 *
+	 * Array
+	 * (
+	 * 		[user_id] => 42
+	 * 		[name] => Kyrylo
+	 * 		[surname] => Hrechenyiuk
+	 * 		[login] => lyrykua
+	 * 		[email] => lyryk.ua@gmail.com
+	 * 		[password] => $2y$10$D.XZmfUrX2mKbCTdQ5bS8OuJx4TjgwO7ExVShDm8FhiGwORAqDp0e
+	 * 		[reg_date] => 2017-09-04 15:22:25
+	 * 		[status] => 1
+	 * 		[forgot_pass] => NULL
+	 * )
+	 *
+	 * @param $user_id
+	 * @return bool|mixed
+	 */
+	protected static function getUserById($user_id)
+	{
+		$row = false;
+		try {
+			$db = static::getDB();
+			$sql = '
+			SELECT *
+			FROM camagru.users
+			WHERE user_id=?;
+			';
+			$stmt = $db->prepare($sql);
+			$stmt->bindParam(1, $user_id);
+			$stmt->execute();
+			$row = $stmt->fetch(\PDO::FETCH_ASSOC);
+		} catch (\PDOException $e) {
+			$e->getMessage();
+		}
+		return $row;
+	}
+
+	/**
+	 * getUserByLogin($login)
+	 *
+	 * This function get an user by `login` in DB's table.
+	 *
+	 * The return value of this function is a row as an array indexed by column name.
+	 * Function returns false, if the user was not found
+	 *
+	 * Array
+	 * (
+	 * 		[user_id] => 42
+	 * 		[name] => Kyrylo
+	 * 		[surname] => Hrechenyiuk
+	 * 		[login] => lyrykua
+	 * 		[email] => lyryk.ua@gmail.com
+	 * 		[password] => $2y$10$D.XZmfUrX2mKbCTdQ5bS8OuJx4TjgwO7ExVShDm8FhiGwORAqDp0e
+	 * 		[reg_date] => 2017-09-04 15:22:25
+	 * 		[status] => 1
+	 * 		[forgot_pass] => NULL
+	 * )
+	 *
+	 * @param $login
+	 * @return bool|mixed
+	 */
 	protected static function getUserByLogin($login)
 	{
+		$row = false;
 		try {
 			$db = static::getDB();
 			$sql = '
@@ -44,29 +120,54 @@ abstract class Model
 			$stmt = $db->prepare($sql);
 			$stmt->bindParam(1, $login);
 			$stmt->execute();
-			$row = $stmt->fetch(\PDO::FETCH_OBJ);
-			return $row;
+			$row = $stmt->fetch(\PDO::FETCH_ASSOC);
 		} catch (\PDOException $e) {
 			$e->getMessage();
 		}
+		return $row;
 	}
 
-	public static function getUserById($id)
+	/**
+	 * getUserByEmail($email)
+	 *
+	 * This function get an user by `email` in DB's table.
+	 *
+	 * The return value of this function is a row as an array indexed by column name.
+	 * Function returns false, if the user was not found
+	 *
+	 * Array
+	 * (
+	 * 		[user_id] => 42
+	 * 		[name] => Kyrylo
+	 * 		[surname] => Hrechenyiuk
+	 * 		[login] => lyrykua
+	 * 		[email] => lyryk.ua@gmail.com
+	 * 		[password] => $2y$10$D.XZmfUrX2mKbCTdQ5bS8OuJx4TjgwO7ExVShDm8FhiGwORAqDp0e
+	 * 		[reg_date] => 2017-09-04 15:22:25
+	 * 		[status] => 1
+	 * 		[forgot_pass] => NULL
+	 * )
+	 *
+	 * @param $email
+	 * @return bool|mixed
+	 */
+	protected static function getUserByEmail($email)
 	{
+		$row = false;
 		try {
 			$db = static::getDB();
 			$sql = '
 			SELECT *
 			FROM camagru.users
-			WHERE user_id=?;
+			WHERE email=?;
 			';
 			$stmt = $db->prepare($sql);
-			$stmt->bindParam(1, $id);
+			$stmt->bindParam(1, $email);
 			$stmt->execute();
-			$row = $stmt->fetch(\PDO::FETCH_OBJ);
-			return $row;
+			$row = $stmt->fetch(\PDO::FETCH_ASSOC);
 		} catch (\PDOException $e) {
 			$e->getMessage();
 		}
+		return $row;
 	}
 }
